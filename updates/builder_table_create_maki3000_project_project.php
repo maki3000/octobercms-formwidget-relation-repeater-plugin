8c0234260646d62b4b04360a336820f54f3c1e82
Maki Mikkelson <maki.mikkelson@gmail.com>
2022-08-14 12:37:38 +0200

    Add new fields

--- updates/builder_table_create_maki3000_project_project.php
+++ updates/builder_table_create_maki3000_project_project.php
@@ -13,7 +13,11 @@ class BuilderTableCreateMaki3000ProjectProject extends Migration
             $table->increments('id')->unsigned();
             $table->string('name');
             $table->string('slug');
-            $table->text('content');
+            $table->float('opacity', 2, 1)->nullable();
+            $table->string('bg_color')->nullable();
+            $table->integer('border_width')->unsigned()->nullable();
+            $table->string('border_color')->nullable();
+            $table->text('content')->nullable();
             $table->text('basics')->nullable();
             $table->integer('sort_order')->unsigned()->default(0);
             $table->timestamp('created_at')->nullable();
